ce1dd466665c824c0181597063bd70d76ed8d0d7
xiaoqgithub <1732100026@163.com>
2018-08-18 12:21:03 +0800

    登录页面和基本表单验证完成

---
+++ public/back/js/login.js
@@ -0,0 +1,39 @@
+$(function(){
+  $("#form").bootstrapValidator({
+      feedbackIcons:{
+        valid: 'glyphicon glyphicon-ok',    // 校验成功
+      invalid: 'glyphicon glyphicon-remove',  // 校验失败
+      validating: 'glyphicon glyphicon-refresh' // 校验中
+      },
+      //配置字段
+      fields:{
+        username:{
+          //配置校验规则
+          validators:{
+            //非空
+            notEmpty:{
+              message:"用户名不能为空"
+            },
+            //长度校验
+            stringLength:{
+              min:2,
+              max:6,
+              message:"用户名长度必须在2-6位"
+            }
+          }
+        },
+        password:{
+          validators:{
+            notEmpty:{
+              message:"密码不能为空"
+            },
+            stringLength:{
+              min:6,
+              max:12,
+              message:"密码长度必须是6-12位"
+            }
+          }
+        }
+      }
+  })
+})
